010d4fbed62d45eef700448e0782d15fca4e9a9f
tonysauvageau <toneeees@gmail.com>
2017-01-31 20:32:44 -0700

    added load file and 404 response

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-var http = require('http'),
-    port = 8080;
+var http = require('http');
+var fs = require('fs');
+
+var port = 8080;
 
 http.createServer( function ( request, response) {
+    
+    fs.readFile('inde.html', function(err, content) {
 
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end("My first node app");
+        if ( err ) {
+            response.writeHead(404, {'Content-Type': 'text/html'});
+
+            fs.readFile('404.html', function(err, content) {
+                response.end(content);
+            });
+
+        }else{
+
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(content);
+            
+        }
+        
+    });
 
 }).listen(port);
 
