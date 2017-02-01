dfeb4d73af8032fb0f1e9ad2e0d434edd0d168bb
tonysauvageau <toneeees@gmail.com>
2017-01-31 19:25:45 -0700

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    port = 8080;
+
+http.createServer( function ( request, response) {
+
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end("My first node app");
+
+}).listen(port);
+
+console.log('server running on port '+port);
